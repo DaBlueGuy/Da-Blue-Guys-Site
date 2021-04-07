f574922c0bed87d16267d746b829bb8a704bb5b4
DaBlueGuy <77519462+DaBlueGuy@users.noreply.github.com>
2021-04-07 14:00:01 -0400

    Add files via upload

--- functions.js
+++ functions.js
@@ -4,5 +4,50 @@ function Slide1(){
     document.getElementById("t3").textContent = "Ma famille";
     document.getElementById("c1").textContent = "Je suis gentil, empathique et curieux.";
     document.getElementById("c2").textContent = "J'aime construire dans Minecraft, programmer en Python et C#, et jouer à des jeux avec mes soeurs.";
-    document.getElementById("c3").textContent = "J'ai ma mère, mon père, mes deux soeurs et mon chien. Mes deux parents sont enseignants.";
+    document.getElementById("c3").textContent = "J'ai ma mère, mon père, mes deux sœurs et mon chien. Mes deux parents sont enseignants.";
+}
+
+function Slide2(){
+    document.getElementById("t1").textContent = "Matières:";
+    document.getElementById("t2").textContent = "Assiette Alimentaire, Fruits et Légumes";
+    document.getElementById("t3").textContent = "Assiette Alimentaire, Boissons et Desserts";
+    document.getElementById("c1").textContent = "J'aime les mathématiques et les sciences. Je n'aime pas les cours d'anglais.";
+    document.getElementById("c2").textContent = "Pour le déjeuner, j'ai des pommes, du houmous et des chips.";
+    document.getElementById("c3").textContent = "J'aime avoir du lait à boire et de la glace comme dessert.";
+}
+
+function Slide3(){
+    document.getElementById("t1").textContent = "";
+    document.getElementById("t2").textContent = "";
+    document.getElementById("t3").textContent = "";
+    document.getElementById("c1").textContent = "";
+    document.getElementById("c2").textContent = "";
+    document.getElementById("c3").textContent = "";
+}
+
+function Slide4(){
+    document.getElementById("t1").textContent = "";
+    document.getElementById("t2").textContent = "";
+    document.getElementById("t3").textContent = "";
+    document.getElementById("c1").textContent = "";
+    document.getElementById("c2").textContent = "";
+    document.getElementById("c3").textContent = "";
+}
+
+function Slide5(){
+    document.getElementById("t1").textContent = "";
+    document.getElementById("t2").textContent = "";
+    document.getElementById("t3").textContent = "";
+    document.getElementById("c1").textContent = "";
+    document.getElementById("c2").textContent = "";
+    document.getElementById("c3").textContent = "";
+}
+
+function Slide6(){
+    document.getElementById("t1").textContent = "";
+    document.getElementById("t2").textContent = "";
+    document.getElementById("t3").textContent = "";
+    document.getElementById("c1").textContent = "";
+    document.getElementById("c2").textContent = "";
+    document.getElementById("c3").textContent = "";
 }
