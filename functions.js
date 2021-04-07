f1fc55f8bcf55a4e9b7c20472c7be678336f66c0
DaBlueGuy <77519462+DaBlueGuy@users.noreply.github.com>
2021-04-07 13:42:44 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -4,5 +4,5 @@ function Slide1(){
     document.getElementById("t3").textContent = "Ma famille";
     document.getElementById("c1").textContent = "Je suis gentil, empathique et curieux.";
     document.getElementById("c2").textContent = "J'aime construire dans Minecraft, programmer en Python et C#, et jouer à des jeux avec mes soeurs.";
-    document.getElementById("c3").textContent = "J'ai ma mère, mon père, mes deux sœurs et mon chien. Mes deux parents sont enseignants.";
+    document.getElementById("c3").textContent = "J'ai ma mère, mon père, mes deux soeurs et mon chien. Mes deux parents sont enseignants.";
 }
