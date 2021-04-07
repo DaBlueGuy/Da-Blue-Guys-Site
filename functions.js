60f508f0ec9267980849ff864827014a82436423
DaBlueGuy <77519462+DaBlueGuy@users.noreply.github.com>
2021-04-07 14:14:26 -0400

    Add files via upload

--- functions.js
+++ functions.js
@@ -2,52 +2,64 @@ function Slide1(){
     document.getElementById("t1").textContent = "Personnalité";
     document.getElementById("t2").textContent = "Centres d'intérêt";
     document.getElementById("t3").textContent = "Ma famille";
+    document.getElementById("t4").textContent = "Matières:";
     document.getElementById("c1").textContent = "Je suis gentil, empathique et curieux.";
     document.getElementById("c2").textContent = "J'aime construire dans Minecraft, programmer en Python et C#, et jouer à des jeux avec mes soeurs.";
     document.getElementById("c3").textContent = "J'ai ma mère, mon père, mes deux sœurs et mon chien. Mes deux parents sont enseignants.";
+    document.getElementById("c4").textContent = "J'aime les mathématiques et les sciences. Je n'aime pas les cours d'anglais.";
 }
 
 function Slide2(){
-    document.getElementById("t1").textContent = "Matières:";
-    document.getElementById("t2").textContent = "Assiette Alimentaire, Fruits et Légumes";
-    document.getElementById("t3").textContent = "Assiette Alimentaire, Boissons et Desserts";
-    document.getElementById("c1").textContent = "J'aime les mathématiques et les sciences. Je n'aime pas les cours d'anglais.";
-    document.getElementById("c2").textContent = "Pour le déjeuner, j'ai des pommes, du houmous et des chips.";
-    document.getElementById("c3").textContent = "J'aime avoir du lait à boire et de la glace comme dessert.";
+    document.getElementById("t1").textContent = "Assiette Alimentaire, Fruits";
+    document.getElementById("t2").textContent = "Assiette Alimentaire, Légumes";
+    document.getElementById("t3").textContent = "Assiette Alimentaire, Boissons";
+    document.getElementById("t4").textContent = "Assiette Alimentaire, Desserts";
+    document.getElementById("c1").textContent = "Pour le déjeuner, je mange des pommes.";
+    document.getElementById("c2").textContent = "Je mange du houmous et des chips.";
+    document.getElementById("c3").textContent = "Je bois du lait.";
+    document.getElementById("c4").textContent = "Je mange de la glace pour le dessert.";
 }
 
 function Slide3(){
     document.getElementById("t1").textContent = "";
     document.getElementById("t2").textContent = "";
     document.getElementById("t3").textContent = "";
+    document.getElementById("t4").textContent = "";
     document.getElementById("c1").textContent = "";
     document.getElementById("c2").textContent = "";
     document.getElementById("c3").textContent = "";
+    document.getElementById("c4").textContent = "";
 }
 
 function Slide4(){
     document.getElementById("t1").textContent = "";
     document.getElementById("t2").textContent = "";
     document.getElementById("t3").textContent = "";
+    document.getElementById("t4").textContent = "";
     document.getElementById("c1").textContent = "";
     document.getElementById("c2").textContent = "";
     document.getElementById("c3").textContent = "";
+    document.getElementById("c4").textContent = "";
 }
 
 function Slide5(){
     document.getElementById("t1").textContent = "";
     document.getElementById("t2").textContent = "";
     document.getElementById("t3").textContent = "";
+    document.getElementById("t4").textContent = "";
     document.getElementById("c1").textContent = "";
     document.getElementById("c2").textContent = "";
     document.getElementById("c3").textContent = "";
+    document.getElementById("c4").textContent = "";
 }
 
 function Slide6(){
     document.getElementById("t1").textContent = "";
     document.getElementById("t2").textContent = "";
     document.getElementById("t3").textContent = "";
+    document.getElementById("t4").textContent = "";
     document.getElementById("c1").textContent = "";
     document.getElementById("c2").textContent = "";
     document.getElementById("c3").textContent = "";
+    document.getElementById("c4").textContent = "";
 }
