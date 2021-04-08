e75dc93bf9653e21f3400bb318418f3380a7bd87
DaBlueGuy <77519462+DaBlueGuy@users.noreply.github.com>
2021-04-08 13:37:45 -0400

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,17 +1,19 @@
 function Slide1(){
-    document.getElementById("title").textContent = "Avril 6";
-    document.getElementById("t1").textContent = "Personnalité";
-    document.getElementById("t2").textContent = "Centres d'intérêt";
+    document.getElementById("link").textContent = "Avril 6";
+    document.getElementById("link").href = "me.jpg";
+    document.getElementById("t1").textContent = "Personnalite";
+    document.getElementById("t2").textContent = "Centres d'interet";
     document.getElementById("t3").textContent = "Ma famille";
-    document.getElementById("t4").textContent = "Matières:";
+    document.getElementById("t4").textContent = "Matieres:";
     document.getElementById("c1").textContent = "Je suis gentil, empathique et curieux.";
-    document.getElementById("c2").textContent = "J'aime construire dans Minecraft, programmer en Python et C#, et jouer à des jeux avec mes soeurs.";
-    document.getElementById("c3").textContent = "J'ai ma mère, mon père, mes deux sœurs et mon chien. Mes deux parents sont enseignants.";
+    document.getElementById("c2").textContent = "J'aime construire dans Minecraft, programmer en Python et C#, et jouer a des jeux avec mes soeurs.";
+    document.getElementById("c3").textContent = "J'ai ma mere, mon pere, mes deux soeurs et mon chien. Mes deux parents sont enseignants.";
     document.getElementById("c4").textContent = "J'aime les mathématiques et les sciences. Je n'aime pas les cours d'anglais.";
 }
 
 function Slide2(){
-    document.getElementById("title").textContent = "Avril 7";
+    document.getElementById("link").textContent = "Avril 7";
+    document.getElementById("link").href = "qwertasdfgzxcvb.png";
     document.getElementById("t1").textContent = "Assiette Alimentaire, Fruits";
     document.getElementById("t2").textContent = "Assiette Alimentaire, Légumes";
     document.getElementById("t3").textContent = "Assiette Alimentaire, Boissons";
@@ -23,7 +25,21 @@ function Slide2(){
 }
 
 function Slide3(){
-    document.getElementById("title").textContent = "Avril 8";
+    document.getElementById("link").textContent = "Avril 8";
+    document.getElementById("link").href = "hary.jpg";
+    document.getElementById("t1").textContent = "Le Film";
+    document.getElementById("t2").textContent = "Mes Vetements";
+    document.getElementById("t3").textContent = "Mes Accessoires";
+    document.getElementById("t4").textContent = "Tarification";
+    document.getElementById("c1").textContent = "Mon ami vient chez moi pour regarder Harry Potter avec moi.";
+    document.getElementById("c2").textContent = "J'ai mon t-shirt, mon sweat-shirt et mon pantalon de survetement.";
+    document.getElementById("c3").textContent = "J'ai mes lunettes, ma bouteille d'eau et mon porte-monnaie pour acheter des snacks.";
+    document.getElementById("c4").textContent = "Les snacks coutent environ 15 dollars, et le film Netflix, environ 10 dollars.";
+}
+
+function Slide4(){
+    document.getElementById("link").textContent = "Avril 9";
+    document.getElementById("link").href = "qwertasdfgzxcvb.png";
     document.getElementById("t1").textContent = "";
     document.getElementById("t2").textContent = "";
     document.getElementById("t3").textContent = "";
@@ -34,8 +50,20 @@ function Slide3(){
     document.getElementById("c4").textContent = "";
 }
 
-function Slide4(){
-    document.getElementById("title").textContent = "Avril 9";
+function Slide5(){
+    document.getElementById("title").textContent = "Avril ";
+    document.getElementById("t1").textContent = "";
+    document.getElementById("t2").textContent = "";
+    document.getElementById("t3").textContent = "";
+    document.getElementById("t4").textContent = "";
+    document.getElementById("c1").textContent = "";
+    document.getElementById("c2").textContent = "";
+    document.getElementById("c3").textContent = "";
+    document.getElementById("c4").textContent = "";
+}
+
+function Slide6(){
+    document.getElementById("title").textContent = "Avril ";
     document.getElementById("t1").textContent = "";
     document.getElementById("t2").textContent = "";
     document.getElementById("t3").textContent = "";
